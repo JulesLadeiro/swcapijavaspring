81e7c396fe38025d5ab45dcc3e894624f9379622
Jules Sorensen <julessorens@gmail.com>
2023-01-30 21:21:43 +0100

    first commit

---
+++ src/main/java/com/swc/swcapi/SwcapiApplication.java
@@ -0,0 +1,13 @@
+package com.swc.swcapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwcapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwcapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swc/swcapi/SwcapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swc.swcapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwcapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
